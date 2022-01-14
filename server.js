51f047fca7c472d1d1ea542b51c4f656bae015f9
Pegasoos <circumambulation24@yahoo.com>
2022-01-14 14:30:49 -0500

    Added logger to server.js and initialized npm

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-//require models once they're made
-
 PORT = process.env.PORT || 3003;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended:true} ));
 app.use(express.json());
 
-app.use(express().static("public"));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
 
